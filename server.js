162134dd9e7aff4b6a92e9868c51fcc06f211026
fjblau <fjblau@gmail.com>
2016-11-30 20:55:24 -0800

    changed server timing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var server = app.listen(3000, function() {
 			console.log('BML now listening on port ' + server.address().port);
 		});
 var io = require('socket.io')(server);
+require('tls').SLAB_BUFFER_SIZE = 100 * 1024 //100Kb
 
 io.on('connection', function(socket){
 	 socket.on('berry', function(msg){
@@ -13,4 +14,4 @@ io.on('connection', function(socket){
 
 setInterval(function() {
 			io.sockets.emit('time', new Date());
-		}, 1000);
+		}, 10);
