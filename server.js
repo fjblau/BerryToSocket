ace4e986d557417121b15fc17818f7b5daa4faa3
fjblau <fjblau@gmail.com>
2016-12-10 19:29:56 -0800

    Added New mqtt data sources

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 var app = require('./app');
+var mqtt = require('mqtt');
+
 var server = app.listen(3000, function() {
 			console.log('BML now listening on port ' + server.address().port);
 		});
 var io = require('socket.io')(server);
+var client = mqtt.connect('tcp://54.173.2.135:1883');
+
 require('tls').SLAB_BUFFER_SIZE = 100 * 1024 //100Kb
 
 io.on('connection', function(socket){
@@ -19,6 +23,22 @@ io.on('connection', function(socket){
 	   io.sockets.emit('strainOut', msg);
 	 });
 	});
+
+client.on('connect', function () {
+  client.subscribe('/sensor/#');
+  client.publish('/sensor/status/', 'Start');
+});
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  io.sockets.emit('mqtt',message.toString());
+});
+
+client.addListener('/sensor/json/', function(topic, payload){
+  sys.puts(topic+'='+payload);
+  io.sockets.emit('mqtt',{'topic':String(topic),
+    'payload':String(payload)});
+});
 
 setInterval(function() {
 			io.sockets.emit('time', new Date());
